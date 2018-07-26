1f1b1f6a1aa227c48a4ea63717b644ebc017215b
Nancerss <nancy.alquicirauc@gmail.com>
2018-07-26 16:18:23 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -31,7 +31,7 @@ end
 #   puts perfect_10_recipe["mini_dark_chocolate_chips"]
 # end
 
-amount_of_chocolate_chips
+# amount_of_chocolate_chips
 
 #3: Use `puts` to print out every ingredient and amount in the hash by iterating through the collection. You'll want to use the `each` method.
 def ingredients_and_amounts(perfect_10_recipe)
